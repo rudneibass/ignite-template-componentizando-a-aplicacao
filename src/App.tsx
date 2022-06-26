35d788a6a12ba5c784ee019da4e3b3027b049c70
Rudnei Nascimento <44809558+rudneibass@users.noreply.github.com>
2022-06-26 17:53:50 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+import { MovieCard } from './components/MovieCard';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import './styles/global.scss';
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function App() {
+
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar selectedGenreId={selectedGenreId} handleClickButton={handleClickButton}/>
+      <Content selectedGenreId={selectedGenreId} selectedGenre={selectedGenre}/>
+    </div>
+  )
+}
